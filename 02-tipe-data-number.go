74cf55fce2a4740f6456ffd8e4afa2c5289d18a3
Abdul Wahid Muhaemin <wamuhabdu@gmail.com>
2022-10-04 18:23:36 +0700

    tipe data number

--- 02-tipe-data-number.go
+++ 02-tipe-data-number.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("Satu =",1)
 	fmt.Println("Dua =",2)
 	fmt.Println("Tiga =",3)
-	fmt.Println("Empat koma 5 =",4,5)
+	fmt.Println("Empat koma 5 =",4.5)
 }
 
 // Secara garis besar tipe data number pada golang terbgi dua
