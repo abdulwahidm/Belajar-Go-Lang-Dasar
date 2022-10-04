392f47e96e1921aa287a209266fa3fab2ea029cc
Abdul Wahid Muhaemin <wamuhabdu@gmail.com>
2022-10-04 17:38:59 +0700

    add helloworld.go

--- 01-helloworld.go
+++ 01-helloworld.go
@@ -6,6 +6,11 @@ func main() {
 	fmt.Println("Hello, World!")
 }
 
-// untuk dapat mengeksekusi program hello world ini, perlu dilakukan kompilasi terlebih dahulu menjadi binary file dengan menjalankan command `go build helloworld.go` pada terminal. kemudian binary file yg telah di build tsb dapat dieksekusi.
+// untuk dapat mengeksekusi program hello world ini, 
+// perlu dilakukan kompilasi terlebih dahulu menjadi binary file 
+// dengan menjalankan command `go build helloworld.go` pada terminal. 
+// kemudian binary file yg telah di build tsb dapat dieksekusi.
 
-// namun untuk tahap pembelajaran, atau development setiap file program berkekstensi .go dapat lansung diekseskusi. Pada direktory terminal perintah yang dijalankan adalah `go run <nama-file.go>`
+// namun untuk tahap pembelajaran, atau development 
+// setiap file program berkekstensi .go dapat lansung diekseskusi. 
+// Pada direktory terminal perintah yang dijalankan adalah `go run <nama-file.go>`
