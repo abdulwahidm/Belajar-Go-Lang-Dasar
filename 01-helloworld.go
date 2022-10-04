14102f4227fc14dea5970323731223d98a17e9cf
Abdul Wahid Muhaemin <wamuhabdu@gmail.com>
2022-10-04 17:34:15 +0700

    update markdown

---
+++ 01-helloworld.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// untuk dapat mengeksekusi program hello world ini, perlu dilakukan kompilasi terlebih dahulu menjadi binary file dengan menjalankan command `go build helloworld.go` pada terminal. kemudian binary file yg telah di build tsb dapat dieksekusi.
+
+// namun untuk tahap pembelajaran, atau development setiap file program berkekstensi .go dapat lansung diekseskusi. Pada direktory terminal perintah yang dijalankan adalah `go run <nama-file.go>`
