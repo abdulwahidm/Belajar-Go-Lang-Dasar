789f380bf20abdac689a6a067106fad963b6056f
Abdul Wahid Muhaemin <wamuhabdul@gmail.com>
2023-07-22 23:19:28 +0700

    map

---
+++ 16-map.go
@@ -0,0 +1,21 @@
+package main
+ 
+import "fmt"
+
+func main() {
+// 	Pada Array atau Slice, untuk mengakses data, kita menggunakan index Number dimulai dari 0
+// Map adalah tipe data lain yang berisikan kumpulan data yang sama, namun kita bisa menentukan jenis tipe data index yang akan kita gunakan
+// Cara menggunakan map cukup dengan menuliskan keyword map diikuti tipe data key dan value-nya.
+// Berbeda dengan Array dan Slice, jumlah data yang kita masukkan ke dalam Map boleh sebanyak-banyaknya, asalkan kata kunci nya berbeda, jika kita gunakan kata kunci sama, maka secara otomatis data sebelumnya akan diganti dengan data baru.
+
+	person := map[string]string{
+		"name" : "Abdul",
+		"address" : "Bekasi",
+	}
+
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["address"])
+
+
+}
