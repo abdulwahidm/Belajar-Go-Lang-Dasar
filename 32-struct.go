4da2ad76b4524b9560393c5e606ca58fa516b3ef
Abdul Wahid Muhaemin <wamuhabdul@gmail.com>
2023-07-24 12:40:21 +0700

    struct literal

--- 32-struct.go
+++ 32-struct.go
@@ -2,10 +2,24 @@ package main
 
 import "fmt"
 
+/**
+Struct adalah sebuah template data yang digunakan untuk menggabungkan nol 
+atau lebih tipe data lainnya dalam satu kesatuan
+Struct biasanya representasi data dalam program aplikasi yang kita buat
+Data di struct disimpan dalam field  
+Sederhananya struct adalah kumpulan dari field
+*/
+
 type Person struct {
 	Name, Address 	string
 	Age				int
 }
+
+/**
+Struct adalah template data atau prototype data
+Struct tidak bisa langsung digunakan
+Namun kita bisa membuat data/object dari struct yang telah kita buat
+*/
 
 func main() {
 	var abdul Person
@@ -16,4 +30,13 @@ func main() {
 	// fmt.Println(abdul.Name)
 	// fmt.Println(abdul.Address)
 	// fmt.Println(abdul.Age)
+
+	// Struct Literal
+	budi := Person {
+		Name : "Budiman",
+		Address: "Jakarta",
+		Age: 21,
+	}
+	 fmt.Println(budi)
 }
+
